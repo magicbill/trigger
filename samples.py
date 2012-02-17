6949ef50019a1f014511da1371ea00932dcfbe71
Scott Purdy <scott@thepurdys.org>
2012-02-16 19:15:46 -0800

    New structure for triggers.  The only requirement is that they are iterators and there are a some base classes to make it easier to write triggers.

--- samples.py
+++ samples.py
@@ -1,24 +1,24 @@
 import datetime
 
-class Print10(object):
-  def __iter__(self):
-    return self
+import trigger
 
-  def next(self, last):
-    return last + datetime.timedelta(seconds=10)
-
+class Print10(trigger.AsyncTriggerBase):
   def execute(self):
     print 10
 
-class Print5(object):
-  def __iter__(self):
-    return self
+  def schedule(self):
+    return datetime.timedelta(seconds=10)
 
-  def next(self, _):
-    return datetime.timedelta(seconds=5)
-
+class Print5(trigger.TriggerBase):
   def execute(self):
     print 5
 
+  def schedule(self):
+    return datetime.timedelta(seconds=5)
+
 def get_triggers():
   return [Print10(), Print5()]
+
+if __name__ == '__main__':
+  server = trigger.TriggerServer(get_triggers())
+  server.run_forever()
--- trigger.py
+++ trigger.py
@@ -5,72 +5,68 @@ return datetime or timedelta instances and additionally have an execute method
 for performing some action.'''
 
 import datetime
-import sys
 import thread
 import time
 
 import Queue
 
-# TODO: Don't hard code this. Figure out how best to handle it.
-import samples
 
-def _get_next(trigger, last=None):
-  if not last:
-    last = datetime.datetime.now()
-  try:
-    expiration = trigger.next(last)
-  except StopIteration:
-    print 'StopIteration'
-    return None
-  if isinstance(expiration, datetime.timedelta):
-    expiration = last + expiration
-  if not isinstance(expiration, datetime.datetime):
-    raise TypeError(
-        'Trigger returned an invalid type "%s" (expected datetime or '
-        'timedelta): %s' % (type(expiration), expiration))
-  return expiration
+class TriggerBase(object):
+  def __iter__(self):
+    return self
 
-def _put_next(trigger, pq, last=None):
-  expiration = _get_next(trigger, last)
-  if expiration is not None:
-    pq.put((expiration, trigger))
-  else:
-    print 'Expiration was none, retiring job: %s' % type(trigger)
+  def execute(self):
+    raise NotImplemented
 
-def _execute_in_thread(trigger):
-  trigger.execute()
+  def schedule(self):
+    raise NotImplemented
 
-def _execute(trigger):
-  thread.start_new_thread(_execute_in_thread, (trigger,))
+  def next(self):
+    self.execute()
+    return self.schedule()
 
-def _next(trigger, pq, last=None):
-  _put_next(trigger, pq, last)
 
-def initialize(triggers):
-  pq = Queue.PriorityQueue()
-  for trigger in triggers:
-    _next(trigger, pq)
-  return pq
+class AsyncTriggerBase(TriggerBase):
+  def next(self):
+    thread.start_new_thread(self.execute, tuple())
+    return self.schedule()
 
-def run(pq):
-  while not pq.empty():
-    expiration, trigger = pq.get()
-    time_left = (expiration - datetime.datetime.now()).total_seconds()
-    if time_left > 0.0:
-      time.sleep(time_left)
-    # Asynchronously execute the trigger.
-    _execute(trigger)
-    # Synchronously get the next trigger expiration and put into PQ.
-    _next(trigger, pq, expiration)
 
-def main(triggers):
-  # Initialize the PQ with all of the triggers.
-  pq = initialize(triggers)
-  # Run forever, or until there are no more triggers.
-  run(pq)
+class TriggerServer(object):
+  def __init__(self, triggers=None):
+    self.pq = Queue.PriorityQueue()
+    now = datetime.datetime.now()
+    for trigger in triggers:
+      self.pq.put((now, trigger))
 
-if __name__ == '__main__':
-  triggers = set()
-  for trigger_module in [samples]:
-    triggers.update(trigger_module.get_triggers())
-  main(triggers)
+  def _process_trigger(self, trigger, last=None):
+    if not last:
+      last = datetime.datetime.now()
+    try:
+      expiration = trigger.next()
+    except StopIteration:
+      print 'Trigger threw StopIteration.'
+      return None
+    if isinstance(expiration, datetime.timedelta):
+      expiration = last + expiration
+    if not isinstance(expiration, datetime.datetime):
+      print ('Trigger returned an invalid type "%s" (expected datetime or '
+             'timedelta): %s' % (type(expiration), expiration))
+      return None
+    return expiration
+
+  def run_one(self, trigger, last=None):
+    expiration = self._process_trigger(trigger, last)
+    if expiration is not None:
+      self.pq.put((expiration, trigger))
+    else:
+      print 'Retiring job: %s' % type(trigger)
+
+  def run_forever(self):
+    while not self.pq.empty():
+      expiration, trigger = self.pq.get()
+      time_left = (expiration - datetime.datetime.now()).total_seconds()
+      if time_left > 0.0:
+        time.sleep(time_left)
+      self.run_one(trigger, expiration)
+    print 'All triggers exhausted.  Exiting.'
